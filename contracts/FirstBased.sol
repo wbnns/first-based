5ecdf2302b433e1706b3b9f679943a603dbdc4f7
wbnns <will.binns@coinbase.com>
2024-01-23 15:29:29 -0100

    Save

--- contracts/FirstBased.sol
+++ contracts/FirstBased.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+import "./@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./@openzeppelin/contracts/access/Ownable.sol";
+import "./@openzeppelin/contracts/utils/Strings.sol";
 import "./library/Base64.sol";
 
 interface IMetadataRenderer {
